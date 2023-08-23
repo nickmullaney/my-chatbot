14b10d11ff28f08b56d7d1358c14633db5a935b9
Nick M <nick.mullaney@gmail.com>
2023-08-23 10:03:04 -0700

    chatbot

---
+++ src/components/chatAvatarButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ChatInterface from "./chatInterface";
+
+const ChatAvatarButton: React.FC = () => {
+    const [isChatOpen, setIsChatOpen] = React.useState(false);
+
+    const toggleChat = () => {
+        setIsChatOpen(!isChatOpen);
+    };
+
+    return(
+        <div>
+            <button onClick={toggleChat}>Chat</button>
+            {isChatOpen && <ChatInterface />}
+        </div>
+    )
+    )
