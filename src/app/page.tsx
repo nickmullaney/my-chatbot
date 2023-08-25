f199f4fe71023d14a3f748799b188e53902b99e3
Nick M <nick.mullaney@gmail.com>
2023-08-25 12:02:42 -0700

    trying out new code and seeing what works for 3.5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
+import Chat from '../components/chat';
 
-import ChatComponent from '@/components/chatComponent';
-
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
-        <h2 className="text-2xl">GPT-3.5 Streaming Chat Application</h2>
-        <ChatComponent />
-      </div>
-    </main>
-  )
+    <Container maxWidth="lg" className="flex flex-col gap-12">
+      <Box className="flex flex-col gap-6">
+        <Typography variant="h1">OpenAI GPT-3 text model usage example</Typography>
+        <Typography className="text-zinc-600">
+          In this example, a simple chat bot is implemented using Next.js, API Routes, and OpenAI API.
+        </Typography>
+      </Box>
+
+      <Box className="flex flex-col gap-3">
+        <Typography variant="h2">AI Chat Bot:</Typography>
+        <Box className="lg:w-2/3">
+          <Chat />
+        </Box>
+      </Box>
+    </Container>
+  );
 }
+
+export default Home;
