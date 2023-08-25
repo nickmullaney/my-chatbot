1f62707088e9ab666ca318acb4ec763728d80854
Nick M <nick.mullaney@gmail.com>
2023-08-25 11:42:24 -0700

    alternate route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-'use client'
-import ChatInterface from '@/components/chatInterface';
-import Image from 'next/image';
 
+import ChatComponent from '@/components/chatComponent';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   
-      <ChatInterface /> {/* Integrate the chat interface component */}
+      <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
+        <h2 className="text-2xl">GPT-3.5 Streaming Chat Application</h2>
+        <ChatComponent />
+      </div>
     </main>
-  );
+  )
 }
